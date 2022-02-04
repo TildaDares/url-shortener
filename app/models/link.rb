ab5b2648975e78f2fe075e909fe051f9d358b55d
TildaDares <mathildaudufo@gmail.com>
2022-02-04 19:04:47 +0100

    generated link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,14 @@
+require 'securerandom'
+
 class Link < ApplicationRecord
+  validates :url, presence: true
+  before_save :generate_slug
+
+  def generate_slug
+    self.slug = SecureRandom.alphanumeric
+  end
+
+  def display_slug
+    ENV['BASE_URL'] + self.slug
+  end
 end

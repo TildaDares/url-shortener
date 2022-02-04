8cc66cfaba2cdad2aea5991682f4427d425a3ba0
TildaDares <mathildaudufo@gmail.com>
2022-02-04 19:04:22 +0100

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :links, only: [:show, :new, :create]
+  root :to => "links#new", as: :home
+  get ':slug' => 'links#show'
 end

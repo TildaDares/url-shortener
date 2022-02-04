ff6bea104f8ce89f0f31a8edec5a671c02eea6eb
TildaDares <mathildaudufo@gmail.com>
2022-02-04 19:05:11 +0100

    controller

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,29 @@
+class LinksController < ApplicationController
+  def show 
+    @link = Link.find_by(slug: params[:slug])
+    if @link
+      redirect_to @link.url
+    end
+  end
+
+  def new 
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new(link_params)
+    respond_to do |format|
+      if @link.save
+        format.js
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:url)
+  end
+end
